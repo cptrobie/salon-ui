ed9d945c9ac3a3694d3744228a34ff340bd9d9b2
cptrobie <Acuity(2)Sw>
2022-09-01 15:25:04 -0400

    Initial commit - LP milestone 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from "./Navigation/Navbar.js";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                <span className="align-middle">
+                    <p>&nbsp;</p>
+                <p className="text-center">
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                </span>
+            </div>
+        </div>);
 }
 
 export default App;
---
+++ src/Navigation/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-fixed-top navbar-dark bg-dark">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Ar Salon &#38; Day Spa</span>
+            </div>
+        </nav>
+    );
+};
+export default Navbar;
